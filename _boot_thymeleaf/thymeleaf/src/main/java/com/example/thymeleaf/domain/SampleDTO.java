570a0ef4f0cd95a41096aaf4c8b8f419e3f47dc2
Bitmul <wlgns7884@gmail.com>
2024-01-22 15:17:01 +0900

    First Commit

---
+++ _boot_thymeleaf/thymeleaf/src/main/java/com/example/thymeleaf/domain/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaf.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SampleDTO {
+	private String p1,p2,p3;
+}
